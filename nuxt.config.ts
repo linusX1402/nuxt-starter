e9583831aaa96baabbe7bc8ab95ae2d64de679f5
linusX1402 <linus@wizany.at>
2024-12-23 16:44:45 +0100

    add: nuxt plugins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: ['@vueuse/nuxt', '@nuxt/fonts', '@nuxt/icon'],
+  icon: {
+    customCollections: [
+      {
+        // ToDo: enter your project name here
+        prefix: 'project-name',
+        dir: './assets/icons'
+      }
+    ]
+  }
 })
